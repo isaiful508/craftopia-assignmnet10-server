cd693a5e474c7bc6087d8a873dfa583700a56595
isaiful508 <isaiful.islam508@gmail.com>
2024-04-26 09:25:18 +0600

    set .env file

--- index.js
+++ index.js
@@ -1,4 +1,6 @@
 const { MongoClient, ServerApiVersion } = require('mongodb');
+require('dotenv').config();
+
 const express = require('express');
 const cors = require('cors');
 const app = express();
@@ -8,13 +10,11 @@ const port = process.env.PORT || 5000;
 app.use(cors());
 app.use(express.json());
 
-//craftopia
-//fYUweLLOht9CgWqH
 
 
-// const uri = "mongodb+srv://craftopia:fYUweLLOht9CgWqH@cluster1.bhtyeej.mongodb.net/?retryWrites=true&w=majority&appName=Cluster1";
 
-const uri = "mongodb+srv://craftopia:fYUweLLOht9CgWqH@cluster1.bhtyeej.mongodb.net/?retryWrites=true&w=majority&appName=Cluster1";
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster1.bhtyeej.mongodb.net/?retryWrites=true&w=majority&appName=Cluster1`;
+
 
 // Create a MongoClient with a MongoClientOptions object to set the Stable API version
 const client = new MongoClient(uri, {
