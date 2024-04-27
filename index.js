091b2407bfb9d01b90879d3266355b00f4884c51
isaiful508 <isaiful.islam508@gmail.com>
2024-04-28 00:45:31 +0600

    view deatils dynamic api created from db

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
-const { MongoClient, ServerApiVersion } = require('mongodb');
+const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
 require('dotenv').config();
 
 const express = require('express');
@@ -32,12 +32,24 @@ async function run() {
 
     const craftCollection = client.db('craftDB').collection('crafts')
 
+
+    app.get('/view_details/:id', async(req, res) =>{
+      const id = req.params.id;
+      const query = {_id : new ObjectId(id)}
+      const craftDetails = await craftCollection.findOne(query)
+      res.send(craftDetails);
+
+    })
+
     app.get('/craft_items', async (req, res) =>{
       const cursor = craftCollection.find();
       const result = await cursor.toArray();
 
       res.send(result);
     })
+
+
+
 
     app.post('/craft_items', async(req, res) =>{
       const newCraftItem = req.body;
